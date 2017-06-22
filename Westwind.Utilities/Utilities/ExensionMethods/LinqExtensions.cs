bdf75162262634af43259e64d52c4e172a7621c8
Rick Strahl <rstrahl@west-wind.com>
2017-06-22 15:54:19 -0700

    Add new LinqExtensions.FlattenTree() function that flattens tree structure containing a fixed child collection property (ie. Children or Topics etc).

---
+++ Westwind.Utilities/Utilities/ExensionMethods/LinqExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Westwind.Utilities.Linq
+{
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Flattens a tree to a plain enumerable for querying.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e">Enumeration to work on</param>
+        /// <param name="f">Expression that points at the element list to select from</param>
+        /// <returns></returns>
+        /// <example>
+        /// var topics = topicTree.FlattenTree(t=> t.Topics);
+        /// </example>
+        public static IEnumerable<T> FlattenTree<T>(
+            this IEnumerable<T> e,
+            Func<T, IEnumerable<T>> f)
+        {
+            return e.SelectMany(c => f(c).FlattenTree(f)).Concat(e);
+        }
+    }
+}
