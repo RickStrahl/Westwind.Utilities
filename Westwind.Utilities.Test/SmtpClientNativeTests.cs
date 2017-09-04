163d4fcfa283f506835daca96a2fd5e6a95b6bf5
Rick Strahl <rstrahl@west-wind.com>
2017-09-04 12:36:45 -0700

    Add simple integration tests for SmtpClientNative

---
+++ Westwind.Utilities.Test/SmtpClientNativeTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Westwind.Utilities.InternetTools;
+
+namespace Westwind.Utilities.Test
+{
+    [TestClass]
+    public class SmtpClientNativeTests
+    {
+        bool isMailServerAvailable = false;
+
+        /// <summary>
+        /// For this test to run make sure Email Server is available
+        /// </summary>
+        [TestMethod]
+        public void SendMailTest()
+        {
+            string errorMessage = null;
+
+            SmtpClientNative smtp = new SmtpClientNative();
+            smtp.MailServer = TestConfigurationSettings.Mailserver;
+            smtp.Username = TestConfigurationSettings.MailServerUsername;
+            smtp.Password = TestConfigurationSettings.MailServerPassword;
+            smtp.UseSsl = TestConfigurationSettings.MailServerUseSsl;
+
+            
+            smtp.SenderEmail = "admin@test.com";
+
+            smtp.Recipient = "test@test.com";
+            smtp.Subject = "Test Message";
+            smtp.Message = "Test Message Content from <b>Test SMTP Client</b>";
+            smtp.ContentType = "text/html";
+            
+            bool result = smtp.SendMail();
+            
+            if (isMailServerAvailable)                
+                Assert.IsTrue(result, smtp.ErrorMessage);
+        }
+
+        /// <summary>
+        /// For this test to run make sure Email Server is available
+        /// </summary>
+        [TestMethod]
+        public void SendMailAsyncTest()
+        {
+            string errorMessage = null;
+
+            SmtpClientNative smtp = new SmtpClientNative();
+            smtp.MailServer = TestConfigurationSettings.Mailserver;
+            smtp.Username = TestConfigurationSettings.MailServerUsername;
+            smtp.Password = TestConfigurationSettings.MailServerPassword;
+            smtp.UseSsl = TestConfigurationSettings.MailServerUseSsl;
+
+
+            smtp.SenderEmail = "admin@test.com";
+
+            smtp.Recipient = "test@test.com";
+            smtp.Subject = "Async Test Message";
+            smtp.Message = "Test Message Content from <b>Async Test SMTP Client</b>";
+            smtp.ContentType = "text/html";
+
+            smtp.SendMailAsync();
+        }
+
+    }
+
+}
--- Westwind.Utilities.Test/_TestConfigurationSettings.cs
+++ Westwind.Utilities.Test/_TestConfigurationSettings.cs
@@ -7,5 +7,13 @@ namespace Westwind.Utilities.Test
 			"server=.;database=WestwindToolkitSamples;integrated security=true;MultipleActiveResultSets=true;";
 		public static string WebStoreConnectionString { get; set; } =
 			"server=.;database=Webstore;integrated security=true;MultipleActiveResultSets=true;";
+
+	    public static string Mailserver { get; set;  } = "localhost";
+
+        public static string MailServerUsername { get; set;  } 
+
+        public static string MailServerPassword { get; set;  }
+
+        public static bool MailServerUseSsl { get; set; }
 	}
 }
