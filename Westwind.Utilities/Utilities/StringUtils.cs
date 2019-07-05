c43d0f1663881642e8281f0114ccc29296430c74
Rick Strahl <rstrahl@west-wind.com>
2019-07-05 11:35:47 +0200

    StringUtils.Right() function

--- Westwind.Utilities/Utilities/StringUtils.cs
+++ Westwind.Utilities/Utilities/StringUtils.cs
@@ -501,6 +501,21 @@ namespace Westwind.Utilities
             return value + terminator;
         }
 
+
+        /// <summary>
+        /// Returns the number or right characters specified
+        /// </summary>
+        /// <param name="full">full string to work with</param>
+        /// <param name="rightCharCount">number of right characters to return</param>
+        /// <returns></returns>
+        public static string Right(string full, int rightCharCount)
+        {
+            if (string.IsNullOrEmpty(full) || full.Length < rightCharCount || full.Length - rightCharCount < 0)
+                return full;
+
+            return full.Substring(full.Length - rightCharCount);
+        }
+
         #endregion
 
         #region String Parsing
