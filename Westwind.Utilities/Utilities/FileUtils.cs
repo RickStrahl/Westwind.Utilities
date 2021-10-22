6c0ec4fdc310a8768be91bae7039c61f28f74e55
Rick Strahl <rstrahl@west-wind.com>
2021-10-22 13:09:18 -0700

    Update GetRelativePath to use Uri.DecodeUrl()

--- Westwind.Utilities/Utilities/FileUtils.cs
+++ Westwind.Utilities/Utilities/FileUtils.cs
@@ -105,12 +105,13 @@ namespace Westwind.Utilities
                 Uri fullUri = new Uri(uriString: fullPath);
 
                 Uri relativeUri = baseUri.MakeRelativeUri(uri: fullUri);
+                
 
                 // Uri's use forward slashes so convert back to backward slahes
                 var path = relativeUri.ToString().Replace(oldValue: "/", newValue: pathChar);
 
 #if !NET40
-                return WebUtility.UrlDecode(path);
+                return Uri.UnescapeDataString(path);
 #else
                 return StringUtils.UrlDecode(path);
 #endif
