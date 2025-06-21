019109be41f8e1eb268594a878d8dbd4c9e2decb
Rick Strahl <rstrahl@west-wind.com>
2025-06-21 16:28:55 -0700

    Add FileUtils.GetWindowsLongFilename for a file

--- Westwind.Utilities/Utilities/FileUtils.cs
+++ Westwind.Utilities/Utilities/FileUtils.cs
@@ -221,6 +221,42 @@ namespace Westwind.Utilities
         );
 
 
+        /// <summary>
+        /// Retrieves a long filename for a given path using
+        /// Extended Windows path syntax.        
+        /// 
+        /// Fully qualified paths:
+        /// \\?\C:\path\to\file.txt
+        /// UNC Paths:
+        /// \\?\UNC\server\share\path\to\file.txt
+        /// </summary>
+        /// <remarks>       
+        /// Paths are resolved using GetFullPath() so preferably
+        /// you should pass in a fully qualified path for your 
+        /// application to ensure that path is resolved correctly.
+        /// </remarks>
+        /// <param name="path">Existing path</param>
+        /// <returns>Long path</returns>
+        public static string GetWindowsLongFilename(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return path;
+
+            string fullPath = System.IO.Path.GetFullPath(path);
+
+            if (fullPath.Length < 260)
+                return fullPath; // No need to convert
+
+            // Fully qualified path
+            if (fullPath.Length > 1 && fullPath[1] == ':')
+                fullPath = @"\\?\" + fullPath;
+            // UNC Path
+            else if (fullPath.Length > 2 && fullPath.StartsWith(@"\\"))
+                fullPath = @"\\?\UNC\" + fullPath.Substring(2);
+
+            return fullPath;
+        }
+
         //// To ensure that paths are not limited to MAX_PATH, use this signature within .NET
         //[DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetShortPathNameW", SetLastError = true)]
         //static extern int GetShortPathName_Internal(string pathName, StringBuilder shortName, int cbShortName);
